1643915634e32da372aa5e91d1763978abf91a78
Maxwell Santos <maxwell.santos@iterative.com.br>
2016-11-11 12:16:52 -0200

    Alura Exercises

---
+++ LacosMultiplos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LacosMultiplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+            for(int i = 1; i <= 100; i++)
+            {
+                if (i % 3 != 0)
+                {
+                    soma += i;
+                }
+            }
+
+            Console.WriteLine("Soma dos numeros de 1 a 100 não multiplos de 3: " + soma);
+
+            Console.ReadKey();
+        }
+    }
+}
